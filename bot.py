069c3cf7e0e1c92b6318a73daeb52c888075885f
Neo <egorgulpa07@gmail.com>
2026-07-30 02:11:02 +0300

    fix: remove High VZP, bring back CENT

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ ROLE_ORDER = [
     RoleInfo('Deputy Owner', 1531246359729016969, ''),
     RoleInfo('High Rank', 1531246359729016968, ''),
     RoleInfo('Medium Rank', 1531246359729016966, ''),
-    RoleInfo('High VZP', 1531246359729016967, ''),
+    RoleInfo('CENT', 1532160268363235438, '', True),
 ]
 
 RECRUIT_ROLE_ID = 1531246359712370817
